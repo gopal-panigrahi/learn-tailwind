af1ab8a23396c391b2ffab7ba2caebfde9015b97
Gopal Panigrahi <50495221+gopal-panigrahi@users.noreply.github.com>
2024-05-02 19:20:25 +0530

    updated the task

--- src/projects/ImageGallery/ImageGallery.tsx
+++ src/projects/ImageGallery/ImageGallery.tsx
@@ -9,27 +9,34 @@ import bookmark from "./assets/bookmark.svg";
 function ImageGallery() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-cyan-50">
-      <div className="m-3 flex flex-col space-y-4 rounded-2xl bg-white p-6 shadow-xl md:p-36">
-        <div className="text-md flex flex-col justify-end space-y-4 text-center md:flex-row md:space-x-8 md:space-y-0">
-          <div className="underline-offset-8 transition-all hover:underline">
-            Vector
+      {/* Card */}
+      <div className="m-3 space-y-10 rounded-3xl bg-white p-6 shadow-2xl md:p-40">
+        {/* Menu */}
+        <div className="flex flex-col items-center justify-center space-y-3 md:mb-24 md:flex-row md:justify-end md:space-x-8 md:space-y-0">
+          {/* MenuItem */}
+          <div className="group">
+            <a href="#">Vector</a>
+            <div className="mx-2 mt-2 border-b-2 border-black opacity-0 duration-200 group-hover:opacity-100" />
           </div>
-          <div className="underline-offset-8 transition-all hover:underline">
-            Illustration
+          <div className="group">
+            <a href="#">Illustration</a>
+            <div className="mx-2 mt-2 border-b-2 border-black opacity-0 duration-200 group-hover:opacity-100" />
           </div>
-          <div className="underline-offset-8 transition-all hover:underline">
-            Images
+          <div className="group">
+            <a href="#">Images</a>
+            <div className="mx-2 mt-2 border-b-2 border-black opacity-0 duration-200 group-hover:opacity-100" />
           </div>
-          <div className="underline-offset-8 transition-all hover:underline">
-            Icons
+          <div className="group">
+            <a href="#">Icons</a>
+            <div className="mx-2 mt-2 border-b-2 border-black opacity-0 duration-200 group-hover:opacity-100" />
           </div>
         </div>
 
-        <div className="flex flex-col space-y-4 md:flex-row md:justify-between md:space-y-0">
-          <div className="flex flex-row border-b border-gray-300">
+        <div className="flex flex-col space-y-5 md:flex-row md:justify-between md:space-y-0">
+          <div className="flex justify-between border-b">
             <input
               type="text"
-              className="p-2 outline-none placeholder:font-light"
+              className="ml-6 border-none placeholder:font-thin focus:outline-none md:w-80"
               placeholder="Search"
             />
             <button>
@@ -37,11 +44,11 @@ function ImageGallery() {
                 xmlns="http://www.w3.org/2000/svg"
                 className="w-8 text-gray-300 duration-200 hover:scale-110"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
                 fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                 <circle cx="10" cy="10" r="7"></circle>
@@ -50,86 +57,99 @@ function ImageGallery() {
             </button>
           </div>
 
-          <button className="rounded-lg bg-black p-3 text-white shadow-lg hover:bg-white hover:text-black hover:outline hover:outline-1 md:w-32">
+          <button className="rounded-md border border-black bg-black px-14 py-3 text-lg font-normal text-white shadow-2xl duration-200 hover:bg-white hover:text-black">
             Upload
           </button>
         </div>
 
-        <div className="flex flex-col flex-wrap gap-4 md:flex-row">
+        {/* Gallery */}
+        <div className="grid gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
           <div className="group relative">
-            <img src={image1} alt="" className="w-72 md:w-48" />
-            <div className="absolute bottom-0 flex w-full flex-row justify-between bg-black bg-opacity-40 p-2 text-xs text-white opacity-0 transition-all group-hover:opacity-100">
-              <div>
-                <p>Abstract Painting</p>
-                <p>245 likes - 35 shares</p>
-              </div>
-              <div className="flex items-center">
-                <img src={bookmark} alt="bookmark" />
+            <img src={image1} alt="" className="w-72" />
+            <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-2 px-4 text-white opacity-0 duration-500 group-hover:opacity-100">
+              <div className="flex w-full justify-between">
+                <div className="font-normal">
+                  <p className="text-sm">Abstract Painting</p>
+                  <p className="text-xs">245 likes - 35 shares</p>
+                </div>
+                <div className="flex items-center">
+                  <img src={bookmark} alt="bookmark" />
+                </div>
               </div>
             </div>
           </div>
 
           <div className="group relative">
-            <img src={image2} alt="" className="w-72 md:w-48" />
-            <div className="absolute bottom-0 flex w-full flex-row justify-between bg-black bg-opacity-40 p-2 text-xs text-white opacity-0 transition-all group-hover:opacity-100">
-              <div>
-                <p>Abstract Painting</p>
-                <p>245 likes - 35 shares</p>
-              </div>
-              <div className="flex items-center">
-                <img src={bookmark} alt="bookmark" />
-              </div>
-            </div>
-          </div>
-
-          <div className="group relative">
-            <img src={image3} alt="" className="w-72 md:w-48" />
-            <div className="absolute bottom-0 flex w-full flex-row justify-between bg-black bg-opacity-40 p-2 text-xs text-white opacity-0 transition-all group-hover:opacity-100">
-              <div>
-                <p>Abstract Painting</p>
-                <p>245 likes - 35 shares</p>
-              </div>
-              <div className="flex items-center">
-                <img src={bookmark} alt="bookmark" />
+            <img src={image2} alt="" className="w-72" />
+            <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-2 px-4 text-white opacity-0 duration-500 group-hover:opacity-100">
+              <div className="flex w-full justify-between">
+                <div className="font-normal">
+                  <p className="text-sm">Abstract Painting</p>
+                  <p className="text-xs">245 likes - 35 shares</p>
+                </div>
+                <div className="flex items-center">
+                  <img src={bookmark} alt="bookmark" />
+                </div>
               </div>
             </div>
           </div>
 
           <div className="group relative">
-            <img src={image4} alt="" className="w-72 md:w-48" />
-            <div className="absolute bottom-0 flex w-full flex-row justify-between bg-black bg-opacity-40 p-2 text-xs text-white opacity-0 transition-all group-hover:opacity-100">
-              <div>
-                <p>Abstract Painting</p>
-                <p>245 likes - 35 shares</p>
-              </div>
-              <div className="flex items-center">
-                <img src={bookmark} alt="bookmark" />
-              </div>
-            </div>
-          </div>
-
-          <div className="group relative">
-            <img src={image5} alt="" className="w-72 md:w-48" />
-            <div className="absolute bottom-0 flex w-full flex-row justify-between bg-black bg-opacity-40 p-2 text-xs text-white opacity-0 transition-all group-hover:opacity-100">
-              <div>
-                <p>Abstract Painting</p>
-                <p>245 likes - 35 shares</p>
-              </div>
-              <div className="flex items-center">
-                <img src={bookmark} alt="bookmark" />
+            <img src={image3} alt="" className="w-72" />
+            <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-2 px-4 text-white opacity-0 duration-500 group-hover:opacity-100">
+              <div className="flex w-full justify-between">
+                <div className="font-normal">
+                  <p className="text-sm">Abstract Painting</p>
+                  <p className="text-xs">245 likes - 35 shares</p>
+                </div>
+                <div className="flex items-center">
+                  <img src={bookmark} alt="bookmark" />
+                </div>
               </div>
             </div>
           </div>
 
           <div className="group relative">
-            <img src={image6} alt="" className="w-72 md:w-48" />
-            <div className="absolute bottom-0 flex w-full flex-row justify-between bg-black bg-opacity-40 p-2 text-xs text-white opacity-0 transition-all group-hover:opacity-100">
-              <div>
-                <p>Abstract Painting</p>
-                <p>245 likes - 35 shares</p>
+            <img src={image4} alt="" className="w-72" />
+            <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-2 px-4 text-white opacity-0 duration-500 group-hover:opacity-100">
+              <div className="flex w-full justify-between">
+                <div className="font-normal">
+                  <p className="text-sm">Abstract Painting</p>
+                  <p className="text-xs">245 likes - 35 shares</p>
+                </div>
+                <div className="flex items-center">
+                  <img src={bookmark} alt="bookmark" />
+                </div>
               </div>
-              <div className="flex items-center">
-                <img src={bookmark} alt="bookmark" />
+            </div>
+          </div>
+
+          <div className="group relative">
+            <img src={image5} alt="" className="w-72" />
+            <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-2 px-4 text-white opacity-0 duration-500 group-hover:opacity-100">
+              <div className="flex w-full justify-between">
+                <div className="font-normal">
+                  <p className="text-sm">Abstract Painting</p>
+                  <p className="text-xs">245 likes - 35 shares</p>
+                </div>
+                <div className="flex items-center">
+                  <img src={bookmark} alt="bookmark" />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="group relative">
+            <img src={image6} alt="" className="w-72" />
+            <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-2 px-4 text-white opacity-0 duration-500 group-hover:opacity-100">
+              <div className="flex w-full justify-between">
+                <div className="font-normal">
+                  <p className="text-sm">Abstract Painting</p>
+                  <p className="text-xs">245 likes - 35 shares</p>
+                </div>
+                <div className="flex items-center">
+                  <img src={bookmark} alt="bookmark" />
+                </div>
               </div>
             </div>
           </div>
